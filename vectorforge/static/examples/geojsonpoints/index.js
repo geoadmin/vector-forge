18956f8426b8a8313c493124d55433b57da0dafb
loicgasser <loicgasser4@gmail.com>
2015-04-17 10:32:03 +0200

    Define a min res

--- vectorforge/static/examples/geojsonpoints/index.js
+++ vectorforge/static/examples/geojsonpoints/index.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   var extent = [420000, 30000, 900000, 350000];
   var defaultResolutions = [4000, 3750, 3500, 3250, 3000, 2750, 2500, 2250,
       2000, 1750, 1500, 1250, 1000, 750, 650, 500, 250, 100, 50, 20, 10, 5,
-      2.5, 2, 1.5, 1, 0.5];
+      2.5, 2];
 
   var wmtsGetTileUrlTemplate =
       'http://wmts{5-9}.geo.admin.ch/1.0.0/{Layer}/default/' +
@@ -174,6 +174,7 @@ $(document).ready(function() {
     view: new ol.View({
       center: [650000, 200000],
       resolution: 250,
+      minResolution: 2,
       extent: extent,
       projection: 'EPSG:21781'
     })
