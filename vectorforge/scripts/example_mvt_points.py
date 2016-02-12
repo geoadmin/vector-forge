7abf2313dd81f36c80931ec1910cd223fcfb5af5
loicgasser <loicgasser4@gmail.com>
2016-02-12 10:33:32 +0000

    Add poolmanager and use in mvt tile creation

--- vectorforge/scripts/example_mvt_points.py
+++ vectorforge/scripts/example_mvt_points.py
@@ -7,6 +7,8 @@ import sys
 import traceback
 import mapbox_vector_tile
 
+from poolmanager import PoolManager
+from multiprocessing import Value
 from shapely.geometry.point import Point
 from shapely.geometry.linestring import LineString
 from shapely.geometry.polygon import Polygon
@@ -19,13 +21,6 @@ from vectorforge.lib.grid import Grid, RESOLUTIONS
 from vectorforge.models.stopo import Vec200Namedlocation
 
 
-t0 = time.time()
-
-model = Vec200Namedlocation
-layerId = model.__bodId__
-DBSession = scoped_session(sessionmaker())
-
-
 def geoadminTileGrid(zoomlevels):
     for zoomLevel in zoomlevels:
         grid = Grid(zoomLevel)
@@ -36,7 +31,7 @@ def geoadminTileGrid(zoomlevels):
         while grid.maxX >= maxX:
             while grid.minY <= minY:
                 tileBounds = [minX, minY, maxX, maxY] = grid.tileBounds(tileCol, tileRow)
-                yield zoomLevel, tileBounds, tileCol, tileRow
+                yield (zoomLevel, tileBounds, tileCol, tileRow)
                 tileRow += 1
             minY = grid.minY
             tileCol += 1
@@ -90,47 +85,33 @@ def quantizeGeomToMVT(geometry, tileExtent):
         return Polygon(qcoords)
 
 
-def toFile(data):
-    with open('test_tile.pbf', 'rw') as f:
-        f.write(data)
-
-
-def main():
-
+skippedTilesCounter = 0
+def createTile(tileSpec):
+    fullPath = None
     try:
-        basePath = '1.0.0/ch.swisstopo.vec200-names-namedlocation/21781/default/'
-        conn = s3Connect('ltgal_aws_admin')
-        bucket = getBucket(conn)
-        counter = 0
-        for zoomLevel, tileExtent, tileCol, tileRow in geoadminTileGrid(range(0, len(RESOLUTIONS[0:24]))):
-            clippedGeometry = model.bboxClippedGeom(tileExtent)
-            query = DBSession.query(model, clippedGeometry)
-            query = query.filter(model.bboxIntersects(tileExtent))
-            features = []
-            for feature in query:
-                properties = feature[0].getProperties()
-                # Add id as used in encoder
-                # https://github.com/mapzen/mapbox-vector-tile/blob/master/mapbox_vector_tile/encoder.py#L110
-                properties['id'] = feature[1].id
-                qgeometry = quantizeGeomToMVT(to_shape(feature[1]), tileExtent)
-                features.append(featureMVT(qgeometry, properties))
-
-            if len(features) > 0:
-                # Test only for now
-                path = '%s/%s/%s.pbf' %(zoomLevel, tileCol, tileRow)
-                mvt = layerMVT(model.__bodId__, features)
-                f = cStringIO.StringIO()
-                f.write(mvt)
-                writeToS3(
-                    bucket, path, gzipFileObject(f), 'vector-forge',
-                    basePath, contentType='application/x-protobuf', contentEnc='gzip')
-                counter += 1
-                if counter % 100 == 0:
-                    t1 = time.time()
-                    ti = t1 - t0
-                    print 'Last tile addresss was %s' % basePath + path
-                    print '%s tiles have been generated so far' % counter
-                    print 'It took %s' %str(datetime.timedelta(seconds=ti))
+        (zoomLevel, tileBounds, tileCol, tileRow) = tileSpec
+        model = Vec200Namedlocation
+        layerBodId = model.__bodId__
+        DBSession = scoped_session(sessionmaker())
+        basePath = '1.0.0/%s/21781/default/' % layerBodId
+        clippedGeometry = model.bboxClippedGeom(tileBounds)
+        query = DBSession.query(model, clippedGeometry)
+        query = query.filter(model.bboxIntersects(tileBounds))
+        features = []
+        for feature in query:
+            properties = feature[0].getProperties()
+            properties['id'] = feature[1].id
+            qgeometry = quantizeGeomToMVT(to_shape(feature[1]), tileBounds)
+            features.append(featureMVT(qgeometry, properties))
+        if len(features) > 0:
+            path = '%s/%s/%s.pbf' %(zoomLevel, tileCol, tileRow)
+            mvt = layerMVT(model.__bodId__, features)
+            f = cStringIO.StringIO()
+            f.write(mvt)
+            writeToS3(
+                bucket, path, gzipFileObject(f), 'vector-forge',
+                basePath, contentType='application/x-protobuf', contentEnc='gzip')
+            fullPath = basePath + path
     except Exception:
         exc_type, exc_value, exc_traceback = sys.exc_info()
         print "*** Traceback:"
@@ -140,11 +121,42 @@ def main():
                                   limit=2, file=sys.stdout)
     finally:
         DBSession.close()
-        t3 = time.time()
-        tf = t3 - t0
-        print 'Tile generation process ended/stopped.'
-        print 'It took %s' % str(datetime.timedelta(seconds=tf))
+    return fullPath
+
+
+def callback(counter, fullPath):
+    if fullPath is None:
+        with skippedTiles.get_lock():
+            skippedTiles.value += 1
+
+    if counter % 1000 == 0:
+        t1 = time.time()
+        ti = t1 - t0
+        if fullPath:
+            print 'Last tile address was %s' % fullPath
+        else:
+            print 'Last tile address was skipped'
+        print '%s tiles have been generated' % (counter - skippedTiles.value)
+        print '%s tiles have been skipped' % skippedTiles.value
+        print 'It took %s' % str(datetime.timedelta(seconds=ti))
+
+
+def main():
+    tileGrid = geoadminTileGrid(range(0, len(RESOLUTIONS[0:24])))
+    pm = PoolManager(numProcs=2)
+    pm.imap_unordered(tileGrid, createTile, 50, callback=callback)
+
+    # End of process
+    t3 = time.time()
+    tf = t3 - t0
+    print 'Tile generation process ended/stopped.'
+    print 'It took %s' % str(datetime.timedelta(seconds=tf))
 
 
 if __name__ == '__main__':
+    t0 = time.time()
+    skippedTiles = Value('i', 0)
+    # Boto is thread safe
+    conn = s3Connect('ltgal_aws_admin')
+    bucket = getBucket(conn)
     main()
