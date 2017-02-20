d4c747c3ade26445ca79a7831c85d1eb39ba6d71
loicgasser <loicgasser4@gmail.com>
2017-02-20 18:35:23 +0100

    Mark individual geojson features based on vector forge config

--- scripts/geojson-modifier.js
+++ scripts/geojson-modifier.js
@@ -1,13 +1,13 @@
 // Command-line tool to modify features in a GeoJSON file.
 // Output is written to stdout.
 //
-// How to run: 
+// How to run:
 //   node geojson_modifier.js \
 //     --infile /data/inputfile.geojson \
 //     --patterns '["key:value"]' \
 //     --tippecanoe_extensions '[{ "maxzoom": 14, "minzoom": 12 }]' \
 //     --outfile /data/outfile.geojson
-// 
+//
 
 var commandLineArgs = require('command-line-args'),
     commandLineUsage = require('command-line-usage'),
@@ -19,6 +19,7 @@ var commandLineArgs = require('command-line-args'),
 const optionDefinitions = [
   { name: 'patterns', alias: 'p', type: String },
   { name: 'tippecanoe_extensions', alias: 't', type: String },
+  { name: 'config', alias: 'c', type: String },
   { name: 'help', alias: 'h', type: Boolean },
   { name: 'infile', type: String },
   { name: 'outfile', type: String, defaultValue: 'modified.geojson' },
@@ -47,6 +48,11 @@ const sections = [
                      '\nExample: \'["KATEGORIE:20"]\''
       },
       {
+        name: 'config',
+        description: 'A json configuration file describing how ' +
+                     'and which features to modify'
+      },
+      {
         name: 'tippecanoe_extensions',
         description: 'Array with tippecanoe extensions to add to features. ' +
                      '\nExample 1: \'[{ "maxzoom": 9, "minzoom": 4 }]\'' +
@@ -70,45 +76,35 @@ const sections = [
 const usage = commandLineUsage(sections);
 
 if (options.help == true || options.infile == undefined ||
-    options.tippecanoe_extensions == undefined) {
+    (options.tippecanoe_extensions == undefined && options.config === undefined)) {
   console.log(usage);
   return;
-} 
+}
 console.log('\nInput:  \t' + options.infile);
-
 // For now we take only the first pattern and extension.
-// TODO: Add logic to handle multiple patterns.
 if (options.patterns) {
   var patternKey = options.patterns === '' ? '' :
       JSON.parse(options.patterns)[0].split(':')[0];
   var patternValue = options.patterns === '' ? '' :
       JSON.parse(options.patterns)[0].split(':')[1];
   console.log('Pattern:\t"' + patternKey + ':' + patternValue + '"');
+} else if (options.config) {
+  var readStreamConfig = fs.createReadStream(options.config, {encoding: 'utf8'});
+  var readJSONStreamConfig = JSONStream.parse();
 }
-var extension = JSON.parse(options.tippecanoe_extensions)[0];
-console.log('Extension:\t' + JSON.stringify(extension));
+
+var extension;
+if (options.tippecanoe_extension) {
+  var extension = JSON.parse(options.tippecanoe_extensions)[0];
+  console.log('Extension:\t' + JSON.stringify(extension));
+}
 
 // Parse GeoJSON with JSONPath features.*.geometry
 var numFeatures = 0, numModifiedFeatures = 0;
-var readStream = fs.createReadStream(options.infile, {enicoding: 'utf8'});
+var readStream = fs.createReadStream(options.infile, {encoding: 'utf8'});
 var readJSONStream = JSONStream.parse('features.*');
 
-
-// Modifies the GeoJSON feature.
-var modifyGeoJSON = function(data) {
-  if (!extension) {
-    // Nothing to change.
-    return data;
-  }
-  if (patternKey && patternValue) {
-    // Does the feature match the pattern?
-    if (data.properties === undefined ||
-        data.properties[patternKey] != patternValue) {
-      return data;
-    }
-  }
-  ++numModifiedFeatures;
-  // Adds a tippecanoe extension.
+var createTippecanoeExtension = function(data) {
   var featureExtension = {};
   for (key in extension) {
     switch(key) {
@@ -130,45 +126,171 @@ var modifyGeoJSON = function(data) {
       default:
     }
   }
-  data.tippecanoe = featureExtension;
-  
+  return featureExtension;
+}
+
+// Modifies the GeoJSON feature.
+var modifyGeoJSON = function(data) {
+  if (!extension && !config) {
+    // Nothing to change.
+    return data;
+  }
+  if (patternKey && patternValue) {
+    // Does the feature match the pattern?
+    if (data.properties === undefined ||
+        data.properties[patternKey] != patternValue) {
+      return data;
+    }
+  }
+  ++numModifiedFeatures;
+  var ext;
+  // Add global extension
+  if (extension) {
+    ext = createTippecanoeExtension(data);
+  } else if (config) {
+    ext = {};
+    // Extension from vectorforge config
+    var dataPropertyValue;
+    var confProperty = config.propertyName;
+    // Property name should be case insensitive
+    if (data.properties.hasOwnProperty(confProperty)) {
+      dataPropertyValue = data.properties[confProperty];
+    } else if (data.properties.hasOwnProperty(confProperty.toUpperCase())) {
+      dataPropertyValue = data.properties[confProperty.toUpperCase()];
+    }
+    if (config.properties.hasOwnProperty(dataPropertyValue)) {
+      ext = config.properties[dataPropertyValue];
+    } else {
+      ext = {
+        minzoom: config.minzoomDefault,
+        maxzoom: config.maxzoomDefault
+      }
+    }
+  }
+  // Adds a tippecanoe extension.
+  data.tippecanoe = ext;
   return data;
 }
 
-// Emits anything from _before_ the first match
-readJSONStream.on('header', function (data) {
-  if (data != undefined) {
-    data.features = [];
-    console.log('\nHeader:\n' + JSON.stringify(data));
-    this.write(data);
-  }
-})
+var readAndWriteGeoJSON = function() {
+  // Emits anything from _before_ the first match
+  readJSONStream.on('header', function (data) {
+    if (data != undefined) {
+      data.features = [];
+      console.log('\nHeader:\n' + JSON.stringify(data));
+      this.write(data);
+    }
+  })
 
-// Emits the (potentially modified) feature.
-readJSONStream.on('data', function(data) {
-  if (++numFeatures % 1000 == 0) {
-      process.stdout.write('Modified ' + numModifiedFeatures / 1000 + 'k of '
-          + numFeatures / 1000 + 'k features\r');
-  }
-  this.write(modifyGeoJSON(data));
-});
+  // Emits the (potentially modified) feature.
+  readJSONStream.on('data', function(data) {
+    if (++numFeatures % 1000 == 0) {
+        process.stdout.write('Modified ' + numModifiedFeatures / 1000 + 'k of '
+            + numFeatures / 1000 + 'k features\r');
+    }
+    this.write(modifyGeoJSON(data));
+  });
 
-// Emits anything from _after_ the last match
-readJSONStream.on('end', function(data) {
-  if (data != undefined) {
-    this.write(data);
-    console.log('Footer:\n' + JSON.stringify(data));
-  }
+  // Emits anything from _after_ the last match
+  readJSONStream.on('end', function(data) {
+    if (data != undefined) {
+      this.write(data);
+      console.log('Footer:\n' + JSON.stringify(data));
+    }
 
-  console.log('\nResults written to \'' + options.outfile + '\'.');
-});
+    console.log('\nResults written to \'' + options.outfile + '\'.');
+  });
 
-var writeStream = fs.createWriteStream(options.outfile);
-// The writeStream expects strings and not JSON objects.
-var jsonToStrings = JSONStream.stringify(
-    open='[\n', sep=',\n', close='\n]\n');
+  var writeStream = fs.createWriteStream(options.outfile);
+  // The writeStream expects strings and not JSON objects.
+  var jsonToStrings = JSONStream.stringify(
+      open='[\n', sep=',\n', close='\n]\n');
 
-readStream
-  .pipe(readJSONStream)
-  .pipe(jsonToStrings)
-  .pipe(writeStream);
+  readStream
+    .pipe(readJSONStream)
+    .pipe(jsonToStrings)
+    .pipe(writeStream);
+}
+
+/*
+  config object structure:
+
+  {filters : [...],
+  propertName: 'objektart',
+  properties:
+   { 'Alpiner Gipfel': { minzoom: 7, maxzoom: 23 },
+     Pass: { minzoom: 9, maxzoom: 23 },
+     Wasserfall: { minzoom: 9, maxzoom: 23 },
+     Hauptgipfel: { minzoom: 10, maxzoom: 23 },
+     Strassenpass: { minzoom: 10, maxzoom: 23 } },
+  minzoom: 7,
+  maxzoom: 23,
+  minzoomDefault: 13,
+  maxzoomDefault: 23 }
+*/
+if (readStreamConfig) {
+  var config = {};
+  readJSONStreamConfig.on('data', function(data) {
+    // Always assume equality operator for now and use only one val to filter
+    var regexp = new RegExp(
+        "([A-Za-z]*)\\s(=|!=|<|<=|>|>=){1}\\s(.*(?=\\s(and|or))|.*)");
+    for (var i=0; i < data.filters.length; i++) {
+      var val = data.filters[i].replace('(', '').replace(')', '');
+      var matches = regexp.exec(val);
+      config.propertyName = matches[1].toLowerCase();
+      if (!config.filters) {
+        config.filters = [];
+      }
+      config.filters.push(matches[3].replace(/\'/g, ''));
+    }
+    // Create min/max zoom for each property
+    var minZoom = Infinity;
+    var maxZoom = -Infinity;
+    var minZoomDefault = Infinity;
+    var maxZoomDefault = -Infinity;
+    config.properties = {};
+    for (var k in data.lods) {
+      minZoom = Math.min(minZoom, parseInt(k));
+      maxZoom = Math.max(maxZoom, parseInt(k));
+      var filterIndices = data.lods[k].filterindices;
+      if (filterIndices) {
+        for (var j=0; j < filterIndices.length; j++) {
+          val = config.filters[filterIndices[j]];
+          if (!config.properties.hasOwnProperty(val)) {
+            config.properties[val] = {
+              minzoom: Math.max(minZoom, parseInt(k)),
+              maxzoom: Math.min(maxZoom, parseInt(k))
+            };
+          } else {
+            config.properties[val].minzoom = Math.min(
+                config.properties[val].minzoom, parseInt(k));
+            config.properties[val].maxzoom = Math.max(
+                config.properties[val].maxzoom, parseInt(k));
+          }
+        }
+      } else {
+        // If no filters are defined the entire dataset is available
+        // at this zoom level
+        for (var prop in config.properties) {
+          config.properties[prop].minzoom = Math.min(
+              config.properties[prop].minzoom, parseInt(k));
+          config.properties[prop].maxzoom = Math.max(
+              config.properties[prop].maxzoom, parseInt(k));
+        }
+        // If filters are not defined (e.g. tile the entire dataset)
+        // We set the default min and max zooms
+        minZoomDefault = Math.min(minZoomDefault, parseInt(k));
+        maxZoomDefault = Math.max(maxZoomDefault, parseInt(k));
+      }
+    }
+    config.minzoom = minZoom;
+    config.maxzoom = maxZoom;
+    config.minzoomDefault = minZoomDefault;
+    config.maxzoomDefault = maxZoomDefault;
+    readAndWriteGeoJSON()
+  });
+  readStreamConfig
+    .pipe(readJSONStreamConfig);
+} else {
+  readAndWriteGeoJSON();
+}
