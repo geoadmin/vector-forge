19ee40d91131bd2f65302ac07d62b7ac1d717ec8
loicgasser <loicgasser4@gmail.com>
2017-02-20 13:35:21 +0100

    Add macro script for tippecanoe conversion

---
+++ scripts/mbtile_tippecanoe.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Usage example:
+# ./scripts/mbtile_tippecanoe.sh ~/data/swissnames/Labels.json 7 22
+
+INFILE=$1
+MIN_ZOOM=$2
+MAX_ZOOM=$3
+
+INFILE_NAME=${INFILE::(-5)}
+
+function setup_tippecanoe {
+  if [ ! -d "${HOME}/tippecanoe" ]; then
+    echo 'Setting up tippecanoe'
+    cd ~
+    git clone https://github.com/mapbox/tippecanoe
+    cd ~/tippecanoe && make
+  else
+    echo 'Skipping tippecanoe installation'
+  fi
+}
+
+function prepare_tippecanoe {
+  if [ ! -f "${INFILE_NAME}_modified.json" ]; then
+    cd "${HOME}/vector-forge"
+    echo "Preparing geojson..."
+    echo "node --harmony scripts/geojson-modifier.js --infile $INFILE --outfile "${INFILE_NAME}_modified.json" --tippecanoe_extensions '[{ "maxzoom": "'${MAX_ZOOM}'", "minzoom": "'${MIN_ZOOM}'"}]'"
+    node --harmony scripts/geojson-modifier.js --infile $INFILE --outfile "${INFILE_NAME}_modified.json" --tippecanoe_extensions '[{ "maxzoom": "'${MAX_ZOOM}'", "minzoom": "'${MIN_ZOOM}'"}]'
+  else
+    echo "Skipping geosjon preparation, ${INFILE_NAME}_modified.json already exists"
+  fi
+}
+
+function process_tippecanoe {
+  if [ ! -f "${INFILE_NAME}.mbtiles" ]; then
+    cd "${HOME}/tippecanoe"
+    echo "Preparing mbtiles..."
+    echo "./tippecanoe -o "${INFILE_NAME}.mbtiles" --force -rg --projection 'EPSG:3857' "${INFILE_NAME}_modified.json""
+    ./tippecanoe -o "${INFILE_NAME}.mbtiles" --force -rg --projection 'EPSG:3857' "${INFILE_NAME}_modified.json"
+  else
+    echo "Skipping tile creations file ${INFILE_NAME}.mbtiles already exists"
+  fi
+}
+
+setup_tippecanoe
+prepare_tippecanoe
+process_tippecanoe
